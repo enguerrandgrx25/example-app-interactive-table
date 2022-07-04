e53f7f8a06a888282b012eb691f73c5069766a62
enguerrandgrx25 <granoux.enguerrand@gmail.com>
2022-07-04 19:03:59 +0200

    change

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,11 +30,14 @@ page_url = "https://www.notion.so/c7ade621ac57491ab4e17a9d8ce01d5d?v=5e82aacac0f
 api_key = "secret_y1reuQYpSgvNA13TKfzKm7Azk9mU4u78dexTXqaPdFi"
 
 
-df = notion_df.download(page_url, api_key=api_key)
+df_all = notion_df.download(page_url, api_key=api_key)
 # Equivalent to: df = pd.read_notion(notion_database_url, api_key=api_key)
 
-df = df[['Name', 'Date', 'Risk Score']]
-df['Date'] = df['Date'].apply(lambda x: str(x).split()[0])
+df_all = df_all[['Name', 'Date', 'Risk Score', "Status"]]
+df_all['Date'] = df_all['Date'].apply(lambda x: str(x).split()[0])
+
+
+df_open = df_all[df_all["Status"] == "Open"]
 #st.dataframe(df)
 
 def color_risk(val):
@@ -53,13 +56,13 @@ def color_risk(val):
 st.markdown('##')
 st.markdown('##')
 
-if("Critique" in df["Risk Score"].to_list()):
+if("Critique" in df_open["Risk Score"].to_list()):
     st.error("Alert vous avez une intrusion Critique")
 
-elif("Majeur" in df["Risk Score"].to_list()):
+elif("Majeur" in df_open["Risk Score"].to_list()):
     st.warning("Alert vous avez une intrusion Majeur")
 
-elif("Modere" in df["Risk Score"].to_list()):
+elif("Modere" in df_open["Risk Score"].to_list()):
     st.info("Alert vous avez une intrusion Modere")
 
 else:
@@ -73,7 +76,8 @@ st.markdown('##')
 
 # Inject CSS with Markdown
 
-df_sort = df.sort_values(['Risk Score'], ascending=False).reset_index(drop=True).style.applymap(color_risk, subset=['Risk Score'])
+df_sort = df_open.sort_values(['Risk Score'], ascending=False).reset_index(drop=True).style.applymap(color_risk, subset=['Risk Score'])
+df_sort_all = df_all.sort_values(['Risk Score'], ascending=False).reset_index(drop=True).style.applymap(color_risk, subset=['Risk Score'])
 
 
 
@@ -89,8 +93,12 @@ hide_dataframe_row_index = """
 st.markdown(hide_dataframe_row_index, unsafe_allow_html=True)
 
 
+if st.checkbox('Display close alert'):
+    st.dataframe(df_sort_all)
+else:
+    st.dataframe(df_sort)
 
-st.dataframe(df_sort)
+
 
 st.markdown('##')
 st.markdown('##')
@@ -98,7 +106,7 @@ st.markdown('##')
 
 col1, col2, col3 = st.columns(3)
 
-col1.metric("Number of detected intrussion", len(df), len(df) - 5)
+col1.metric("Number of detected intrussion", len(df_all), len(df_all) - 5)
 col2.metric(
     "Tracked assets",
     round(20),
@@ -109,7 +117,7 @@ st.markdown('##')
 st.markdown('##')
 
 fig = plt.figure(figsize=(10, 4))
-sns.countplot(x="Risk Score", data=df)
+sns.countplot(x="Risk Score", data=df_all)
 st.pyplot(fig)
 
 
